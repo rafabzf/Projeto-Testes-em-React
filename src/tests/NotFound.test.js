b11e218026bc651af647b7d0290952312073d857
Rafael <rafinhabzf@gmail.com>
2022-08-18 14:14:33 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testa a aplicação NotFound', () => {
+  test('Testa se a página contém um h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const textH2 = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/i });
+
+    expect(textH2).toBeInTheDocument();
+  });
+
+  test('Testa se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
